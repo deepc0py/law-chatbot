37924b44b24b4bfc1ecf275cc84f27ef9f85e584
tylerjheslop <tylerjheslop@gmail.com>
2025-04-06 19:21:00 -0700

    chatbot

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+// import { ThemeProvider } from "@/components/providers";
+import { cn } from "@/lib/utils";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Inter } from "next/font/google";
+import { ReactNode } from "react";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+// export const metadata = {
+//   title: "OpenAI and AI SDK Chatbot",
+//   description: "A simple chatbot built using the AI SDK and gpt-4o-mini.",
+// };
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={cn("flex min-h-svh flex-col antialiased", inter.className)}>
+        {/* <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange> */}
+        <TooltipProvider delayDuration={0}>{children}</TooltipProvider>
+        {/* </ThemeProvider> */}
+      </body>
+    </html>
+  );
+}
+
